d08aabdf31699c5e77e00d11a4c8fa3929cd5fd1
Quoc Hung <hungnpoil@gmail.com>
2025-04-23 16:52:46 +0700

    feat: scraper dùng SerpAPI lấy dữ liệu Google Maps

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import os
 from serpapi.google_search_results import GoogleSearch
 
-SERP_API_KEY = os.getenv("SERPAPI_API_KEY")  # Bạn phải set biến môi trường này trên Render
+SERP_API_KEY = os.getenv("4d1768c9601cd2fb5b7e78d33f981521d07751a2a73e34fe31e09865e86a9be8")  # Bạn phải set biến môi trường này trên Render
 
 def scrape_from_keywords(keywords):
     all_results = []
