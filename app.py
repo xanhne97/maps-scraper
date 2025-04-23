dc7ce50f819213eecc9dcd0204ea24de0603ac05
Quoc Hung <hungnpoil@gmail.com>
2025-04-23 17:34:01 +0700

    fix:cap nhat noi dung

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ app = Flask(__name__)
 def index():
     data = []
     if request.method == "POST":
-        keyword_str = request.form.get("keywords", "")
-        keywords = [kw.strip() for kw in keyword_str.splitlines() if kw.strip()]
-        if keywords:
-            data = scrape_from_keywords(keywords)
+        try:
+            keyword_str = request.form.get("keywords", "")
+            keywords = [kw.strip() for kw in keyword_str.splitlines() if kw.strip()]
+            if keywords:
+                data = scrape_from_keywords(keywords)
+        except Exception as e:
+            print("❌ Lỗi tìm kiếm:", e)
     return render_template("index.html", data=data)
 
 if __name__ == "__main__":
