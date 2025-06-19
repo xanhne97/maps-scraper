4984582dab6a56bd27e5720cc7067f5f5f425d08
Quoc Hung <hungnpoil@gmail.com>
2025-06-19 09:24:05 +0700

    cap nhat api

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from serpapi import GoogleSearch
 from geopy.distance import geodesic
 
 # Lấy API key từ biến môi trường hoặc giá trị mặc định (nên đặt biến môi trường SERPAPI_API_KEY trên Render)
-SERP_API_KEY = os.getenv("SERPAPI_API_KEY") or "465eb3451bbe275d8c0a1187cd514ab638f19ae37b6d4f6cade64cb91090023f"
+SERP_API_KEY = os.getenv("SERPAPI_API_KEY") or "2feabda4620a017c8408e7de1e74d72c22f1d78830d0a43b90cab4db83fbddd8"
 
 # Vị trí trung tâm mặc định (có thể thay đổi theo input người dùng)
 CENTER_COORDS = (10.9501, 106.8167)  # Ví dụ: Biên Hòa
